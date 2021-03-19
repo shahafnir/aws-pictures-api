ce3fd4a93c5bfa99684f9cc7bb85d032a339db69
Nir Shahaf <nirshahaf@Nirs-MacBook-Pro.local>
2021-03-19 10:41:06 +0200

    finished picture router

--- src/models/picture.js
+++ src/models/picture.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose')
 
-const pictureSchema = new mongoose.Schema({
-    data: mongoose.Schema.Types.Buffer,
-})
+const pictureSchema = new mongoose.Schema(
+    {
+        description: {
+            type: String,
+            default: 'Picture',
+        },
+        data: {
+            type: Buffer,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Picture = mongoose.model('Picture', pictureSchema)
+
+module.exports = Picture
--- src/routers/picture.js
+++ src/routers/picture.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const Picture = require('../models/picture')
+const multer = require('multer')
+const sharp = require('sharp')
 
 const router = new express.Router()
 
-router.post('/pictures', async (req, res) => {
-    const picture = new Picture(req.body)
+const upload = multer({
+    limits: {
+        fileSize: 3000000,
+    },
+    fileFilter(req, file, callback) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return callback(
+                new Error('Only jpg, jpeg and png files are allowed')
+            )
+        }
 
+        callback(undefined, true)
+    },
+})
+
+router.post('/pictures', upload.single('file'), async (req, res) => {
     try {
+        const buffer = await sharp(req.file.buffer)
+            .resize({ width: 200, height: 360 })
+            .png()
+            .toBuffer()
+
+        const picture = new Picture({
+            description: req.body.description,
+            data: buffer,
+        })
+
         await picture.save()
 
         res.status(201).send(picture)
@@ -28,4 +53,21 @@ router.get('/pictures', async (req, res) => {
 router.get('/', async (req, res) => {
     res.redirect('pictures')
 })
+
+router.delete('/pictures/:id', async (req, res) => {
+    pictureId = req.params.id
+
+    try {
+        const picture = await Picture.findByIdAndDelete(pictureId)
+
+        if (!picture) {
+            return res.status(404).send()
+        }
+
+        res.send(picture)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
 module.exports = router
